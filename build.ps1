b289fb23d4a31c8ec2a2353248c5c4d583a29254
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2026-01-05 12:50:49 +0700

    Update

---
+++ build.ps1
@@ -0,0 +1,20 @@
+# $folder = "src\T01_intro"
+# npm run html $folder
+# npm run pdf $folder
+
+# $folder = "src\T04_flutter"
+# npm run html $folder
+# npm run pdf $folder
+
+# $folder = "src\T05_stateful_widget"
+# npm run html $folder
+# npm run pdf $folder
+
+# $folder = "src\T06_inherited_widget"
+# npm run html $folder
+# npm run pdf $folder
+
+$folder = "src\T07_constraints"
+npm run html $folder
+npm run pdf $folder
+
